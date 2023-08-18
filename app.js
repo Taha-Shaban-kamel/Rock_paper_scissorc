d4d43669e3696187e084de31c27115a50202f6f7
Taha-Shaban-kamel <122947761+Taha-Shaban-kamel@users.noreply.github.com>
2023-08-18 16:15:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const move =document.querySelector('.js-result');
 let score= JSON.parse(localStorage.getItem('score')) || {
   wins:0,
   losses:0,
@@ -23,52 +24,52 @@ let score= JSON.parse(localStorage.getItem('score')) || {
 
   if(playerMove=='scissors'){
     if(computerMove === 'rock'){
-      result='You lose.'
+      result='You lose 😌 .'
     }else if(computerMove==='paper'){
-      result ='You win.';
+      result ='You win 😎 .';
     }else if(computerMove === 'scissors'){
-      result ='Tie.';
+      result ='Tie 😶 .';
     }
 
   }else if(playerMove==='rock'){
         if(computerMove === 'rock'){
-      result='Tie.'
+      result='Tie 😶 .'
       }else if(computerMove==='paper'){
-        result='You lose.';
+        result='You lose 😌 .';
       }else if(computerMove === 'scissors'){
-        result ='You win.';
+        result ='You win 😎 .';
       }
        
   }else if(playerMove==='paper'){
       if(computerMove === 'rock'){
-        result='You win.'
+        result='You win 😎 .'
       }else if(computerMove==='paper'){
-        result='Tie.';
+        result='Tie 😶 .';
       }else if(computerMove === 'scissors'){
-        result ='You lose.';
+        result ='You lose 😌 .';
       }
     }
-    if(result==='You win.')
+    if(result==='You win 😎 .')
     {
       score.wins+=1;
-    }else if(result === 'You lose.')
+    }else if(result === 'You lose 😌 .')
     {
       score.losses+=1;
-    }else if(result==='Tie.'){
+    }else if(result==='Tie 😶 .'){
       score.ties+=1;
     }
     localStorage.setItem('score',JSON.stringify(score));
     updateScore();
-    // alert(`You picked ${playerMove} computer picked ${computerMove} . ${result}`);
     const resul=document.querySelector('.js-moves').innerHTML=`You
   <img src="img/${playerMove}-emoji.png"alt="" class="move-icon">
   <img src="img/${computerMove}-emoji.png" alt="" class="move-icon">
   Computer`;
-    const move =document.querySelector('.js-result').innerHTML=result;
+    move.innerHTML = result;
     
   }
   function updateScore(){
     document.querySelector('.js-score')
     .innerHTML=`Wins ${score.wins} , Losses ${score.losses} ,Ties ${score.ties}`;
+    move.innerHTML = '';
   }
   
